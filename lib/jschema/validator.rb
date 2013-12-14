62eca1ea6ab31b94afcae8b770c3a94b04beb922
Konstantin Papkovskiy <konstantin@papkovskiy.com>
2013-12-15 02:31:54 +0400

    Refactor Validator.build a bit

--- lib/jschema/validator.rb
+++ lib/jschema/validator.rb
@@ -1,17 +1,14 @@
 module JSchema
   module Validator
     def self.build(schema, parent)
-      validators = []
       if schema.is_a?(Hash)
-        constants.each do |validator_class_sym|
+        constants.map do |validator_class_sym|
           validator_class = const_get(validator_class_sym)
-          if (validator = validator_class.build(schema, parent))
-            validators << validator
-          end
-        end
+          validator_class.build(schema, parent)
+        end.compact
+      else
+        []
       end
-
-      validators
     end
   end
 end

96e4f2dae946044090956271b23adc3a632675fc
Konstantin Papkovskiy <konstantin@papkovskiy.com>
2014-01-02 00:19:01 +0400

    Refactor maxProperties and minProperties validators

--- lib/jschema/validator/max_properties.rb
+++ lib/jschema/validator/max_properties.rb
@@ -1,31 +1,5 @@
 module JSchema
   module Validator
-    class MaxProperties < SimpleValidator
-      private
-
-      self.keywords = ['maxProperties']
-
-      def validate_args(max_properties)
-        if greater_or_equal_to?(max_properties, 0)
-          true
-        else
-          invalid_schema 'maxProperties', max_properties
-        end
-      end
-
-      def post_initialize(max_properties)
-        @max_properties = max_properties
-      end
-
-      def applicable_types
-        [Hash]
-      end
-
-      def validate_instance(instance)
-        if instance.keys.size > @max_properties
-          "#{instance} has too many properties"
-        end
-      end
-    end
+    MaxProperties = JSchema::PropertiesSizeValidator('maxProperties', '>')
   end
 end
--- lib/jschema/validator/min_properties.rb
+++ lib/jschema/validator/min_properties.rb
@@ -1,31 +1,5 @@
 module JSchema
   module Validator
-    class MinProperties < SimpleValidator
-      private
-
-      self.keywords = ['minProperties']
-
-      def validate_args(min_properties)
-        if greater_or_equal_to?(min_properties, 0)
-          true
-        else
-          invalid_schema 'minProperties', min_properties
-        end
-      end
-
-      def post_initialize(min_properties)
-        @min_properties = min_properties
-      end
-
-      def applicable_types
-        [Hash]
-      end
-
-      def validate_instance(instance)
-        if instance.keys.size < @min_properties
-          "#{instance} has too few properties"
-        end
-      end
-    end
+    MinProperties = JSchema::PropertiesSizeValidator('minProperties', '<')
   end
 end

46141e11e3382448cab3ee2d7e13f9877e4c3401
Konstantin Papkovskiy <konstantin@papkovskiy.com>
2014-05-30 00:53:30 +0400

    Revert "Refactor maxItems and minItems validators"

--- lib/jschema/validator/min_items.rb
+++ lib/jschema/validator/min_items.rb
@@ -1,5 +1,28 @@
 module JSchema
   module Validator
-    MinItems = JSchema::ItemsSizeValidator('minItems', '<')
+    class MinItems < SimpleValidator
+      private
+
+      self.keywords = ['minItems']
+
+      def validate_args(min_items)
+        greater_or_equal_to?(min_items, 0) ||
+          invalid_schema('minItems', min_items)
+      end
+
+      def post_initialize(min_items)
+        @min_items = min_items
+      end
+
+      def validate_instance(instance)
+        if instance.size < @min_items
+          "#{instance} has too few items"
+        end
+      end
+
+      def applicable_types
+        [Array]
+      end
+    end
   end
 end

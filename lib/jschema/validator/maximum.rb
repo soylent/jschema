706080a62f0bef74680d959f81236057a1fd2081
Konstantin Papkovskiy <konstantin@papkovskiy.com>
2014-05-30 00:54:00 +0400

    Revert "Refactor minimum and maximum validators"

--- lib/jschema/validator/maximum.rb
+++ lib/jschema/validator/maximum.rb
@@ -1,5 +1,31 @@
 module JSchema
   module Validator
-    Maximum = JSchema::LimitValidator('maximum', '<')
+    class Maximum < SimpleValidator
+      private
+
+      self.keywords = ['maximum', 'exclusiveMaximum']
+
+      def validate_args(maximum, exclusive_maximum)
+        number?(maximum) || invalid_schema('maximum', maximum)
+        exclusive_maximum.nil? || boolean?(exclusive_maximum) ||
+          invalid_schema('exclusiveMaximum', exclusive_maximum)
+      end
+
+      def post_initialize(maximum, exclusive_maximum)
+        @maximum = maximum
+        @exclusive_maximum = exclusive_maximum
+      end
+
+      def validate_instance(instance)
+        method = @exclusive_maximum ? :< : :<=
+        unless instance.public_send(method, @maximum)
+          "#{instance} must be #{method} than #{@maximum}"
+        end
+      end
+
+      def applicable_types
+        [Fixnum, Bignum, Float, BigDecimal]
+      end
+    end
   end
 end

597e8025b9b77ea27b73db0b4c4070033f265499
Konstantin Papkovskiy <konstantin@papkovskiy.com>
2014-09-07 16:52:41 +0400

    Refactor simple validator a bit

--- lib/jschema/simple_validator.rb
+++ lib/jschema/simple_validator.rb
@@ -28,7 +28,7 @@ module JSchema
     end
 
     def validate(instance)
-      if !applicable_types || is_type_applicable?(instance.class)
+      if !applicable_types || applicable_types.any? { |type| type === instance }
         validate_instance(instance)
       end
     end
@@ -81,10 +81,6 @@ module JSchema
       schema.is_a?(Hash) && Schema.build(schema, parent, id)
     rescue InvalidSchema
       false
-    end
-
-    def is_type_applicable?(type)
-      applicable_types.any? { |t| type.ancestors.include? t }
     end
   end
 end
